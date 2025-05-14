6953d8bea61691bf3010d1a1f7d8b0f1e895cebf
kobayashi_yuzuri <kobayashi.yuzuri@alhinc.jp>
2025-05-14 10:52:54 +0900

    5/14 AM10:52更新しました

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -29,34 +29,33 @@ public class CalculateSales {
 	 *
 	 * @param コマンドライン引数
 	 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		// 支店コードと支店名を保持するMap
 		Map<String, String> branchNames = new HashMap<>();
 		// 支店コードと売上金額を保持するMap
 		Map<String, Long> branchSales = new HashMap<>();
 		// 支店定義ファイル読み込み処理
-		if(!readFile(args[0], FILE_NAME_BRANCH_LST, branchNames, branchSales)){
+		if(!readFile(args[0], FILE_NAME_BRANCH_LST, branchNames, branchSales)) {
 			return;
 		}
 		File[] files = new File(args[0]).listFiles();
 		List<File> rcdFiles = new ArrayList<>();
 
-		for(int i = 0; i < files.length; i++){
+		for(int i = 0; i < files.length; i++) {
 			String fileName = files[i].getName();
-			if(fileName.matches("^[0-9]{8}.rcd$")){
+			if(fileName.matches("^[0-9]{8}.rcd$")) {
 				rcdFiles.add(files[i]);
 			}
 		}
 		//ファイル数分繰り返す
-		for(int i = 0; i < rcdFiles.size(); i++){
+		for(int i = 0; i < rcdFiles.size(); i++) {
 			BufferedReader br = null;
-			try{
+			try {
 				//rcdFilesを読み取り
 				FileReader fr = new FileReader(rcdFiles.get(i));
 				br = new BufferedReader(fr);
 				String line;
 				List <String> saleList = new ArrayList<String>();
-				//ファイルデータの2行の文字列をキーとバリューに分割
 				//売り上げファイルの行数分繰り返す(2回文)
 				while((line = br.readLine()) != null) {
 					saleList.add(line);
@@ -65,24 +64,24 @@ public class CalculateSales {
 				Long saleAmount = branchSales.get(saleList.get(0)) + fileSale;
 				//更新した支店の売り上げをMapに格納
 				branchSales.put(saleList.get(0), saleAmount);
-			}catch(IOException e){
+			} catch(IOException e) {
 				System.out.println(UNKNOWN_ERROR);
-			}finally{
-				if(br != null){
+				return;
+			} finally {
+				if(br != null) {
 					try {
 						br.close();
-					} catch (IOException e) {
+					} catch(IOException e) {
 						System.out.println(UNKNOWN_ERROR);
-						return ;
+						return;
 					}
 				}
 			}
-		// 支店別集計ファイル書き込み処理
-			if(!writeFile(args[0], FILE_NAME_BRANCH_OUT, branchNames, branchSales)){
-			return;
-			}
 		}
-
+		// 支店別集計ファイル書き込み処理
+		if(!writeFile(args[0], FILE_NAME_BRANCH_OUT, branchNames, branchSales)) {
+			return;
+		}
 	}
 
 	/**
@@ -97,8 +96,10 @@ public class CalculateSales {
 	private static boolean readFile(String path, String fileName, Map<String, String> branchNames, Map<String, Long> branchSales) {
 		BufferedReader br = null;
 
-		try{
+		try {
 			File file = new File(path, fileName);
+			//ファイルが存在しない場合の処理1-1
+			//ファイルのフォーマットが不正の場合の処理1-1
 			FileReader fr = new FileReader(file);
 			br = new BufferedReader(fr);
 			String line;
@@ -108,21 +109,20 @@ public class CalculateSales {
 				branchNames.put(items[0], items[1]);
 			    branchSales.put(items[0], (long)0);
 			}
-		}catch(IOException e) {
-			System.out.println(FILE_NOT_EXIST);
+		} catch(IOException e) {
+			System.out.println(UNKNOWN_ERROR);
 			return false;
-		}finally{
+		} finally {
 			// ファイルを開いている場合
 			if(br != null) {
 				try {
 					// ファイルを閉じる
 					br.close();
-				}catch(IOException e){
+				} catch(IOException e) {
 					System.out.println(UNKNOWN_ERROR);
 					return false;
 				}
 			}
-
 		}
 		return true;
 	}
@@ -140,13 +140,13 @@ public class CalculateSales {
 
 		BufferedWriter bw = null;
 
-		try{
+		try {
 			//書き込み先の指定
 			File file = new File(path, fileName);
 			FileWriter fw = new FileWriter(file);
 			bw = new BufferedWriter(fw);
 			//すべての支店を書き込むまで繰り返し
-			for(String key : branchNames.keySet()){
+			for(String key : branchNames.keySet()) {
 				//支店の書き込み
 				bw.write(key);
 				bw.write(",");
@@ -154,17 +154,16 @@ public class CalculateSales {
 				bw.write(",");
 				String longString = String.valueOf(branchSales.get(key));
 				bw.write(longString);
-				//改行
 				bw.newLine();
 			}
-		}catch(IOException e){
+		} catch(IOException e) {
 			System.out.println(UNKNOWN_ERROR);
 			return false;
-		}finally{
-			if(bw != null){
+		} finally {
+			if(bw != null) {
 				try {
 					bw.close();
-				} catch (IOException e){
+				} catch (IOException e) {
 					System.out.println(UNKNOWN_ERROR);
 					return false;
 				}
